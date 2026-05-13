b85df0a92c6ac13081ecd4ee951330906ed0f49d
SithilSemitha <sithilsemithad@gmail.com>
2026-05-13 11:27:41 +0530

    feat: Updated App.js to handle errors plus controllers

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 import express, { json } from 'express';
+import cookieParser from 'cookie-parser';
+
 import { PORT } from './config/env.js';
 import authRouter  from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import  subscriptionRouter  from './routes/subscription.routes.js';
+import connectToTheDatabase  from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
-app.use(express(json));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
-
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
-app.use('/api/v1/subscriptions',subscriptionRouter);
+app.use(errorMiddleware);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, async () => {
     console.log(`Subscription API is Sucessfully running on http://localhost:${PORT}`)
+    await connectToTheDatabase();
 })
 
 export default app;
