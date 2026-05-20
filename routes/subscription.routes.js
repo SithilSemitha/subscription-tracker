7d825c444c0e933a05a1e1212aab6c668c8771c8
SithilSemitha <sithilsemithad@gmail.com>
2026-05-20 17:06:09 +0530

    feat: updated Subcsription Routes

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import authorize from "../middleware/auth.middleware.js";
+import { createSubscription, getUserSubcriptions } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -10,9 +12,7 @@ subscriptionRouter.get('/:id', (req,res) => {
     res.send({message: "GET ALL SUBSCRIPTIONS BY ID"});
 })
 
-subscriptionRouter.post('/', (req,res) => {
-    res.send({message: "CREATE SUBSCRIPTIONS"});
-})
+subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req,res) => {
     res.send({message: "UPDDATE SUBSCRIPTIONS"});
@@ -22,9 +22,7 @@ subscriptionRouter.delete('/:id', (req,res) => {
     res.send({message: "DELETE SUBSCRIPTIONS"});
 })
 
-subscriptionRouter.get('/user/:id', (req,res) => {
-    res.send({message: "GET ALL SUBSCRIPTIONS FOR USER"});
-})
+subscriptionRouter.get('/user/:id', authorize, getUserSubcriptions);
 
 subscriptionRouter.put('/:id/cancel', (req,res) => {
     res.send({message: "CANCVEL SUBSCRIPTION"});
